a3515e373cb31034ecdf5a2b5ba814def6d180e1
butebaliyeva <utebaliyevabotakoz@gmail.com>
2023-01-23 22:26:51 +0600

    testLogging

---
+++ src/test/java/tests/simple/DisabledTests.java
@@ -0,0 +1,23 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("smoke")
+public class DisabledTests {
+    @Test
+    @Disabled
+    void test1() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled("Any text here")
+    void test2() {
+        assertTrue(false);
+    }
+
+}
