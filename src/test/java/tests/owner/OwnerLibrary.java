12f5fa407399aa45d912473d11eecd498dab10b2
butebaliyeva <utebaliyevabotakoz@gmail.com>
2023-02-19 17:23:37 +0600

    ownerLibrary updated

--- src/test/java/tests/owner/OwnerLibrary.java
+++ src/test/java/tests/owner/OwnerLibrary.java
@@ -5,6 +5,10 @@ import org.junit.Before;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OwnerLibrary {
@@ -16,13 +20,14 @@ public class OwnerLibrary {
 
     @Test
     @Tag("owner")
-    void test1() {
-        assertTrue(true);
+    void shouldFindWiki() {
+        // открыть браузер на странице гитхаб
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".wiki-rightbar").$$("li").last().$("button").click();
+        $(".wiki-rightbar").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
+
     }
 
-    @Test
-    @Tag("owner")
-    void test2() {
-        assertTrue(true);
-    }
 }
